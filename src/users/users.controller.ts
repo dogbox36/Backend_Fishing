34fd8e7cde3178424909e878a9e9166934b90806
Moravcsik Márk <Moravcsikmark@gmail.com>
2023-03-07 13:11:24 +0100

    Request javitva

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   UseGuards,
+  Request,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
